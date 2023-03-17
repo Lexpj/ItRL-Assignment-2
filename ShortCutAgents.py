444a49cb7e653cd7f8bf959d43186ebc5abb42a2
L.P. Janssens <l.p.janssens@umail.leidenuniv.nl>
2023-03-17 22:15:58 +0100

    added agents and general run_repetitions

--- ShortCutAgents.py
+++ ShortCutAgents.py
@@ -24,17 +24,17 @@ class SARSAAgent(object):
         self.n_actions = n_actions
         self.n_states = n_states
         self.epsilon = epsilon
-        # TO DO: Add own code
-        pass
+        self.Q = np.zeros((n_states, n_actions))
         
     def select_action(self, state):
-        # TO DO: Add own code
-        a = random.choice(range(self.n_actions)) # Replace this with correct action selection
-        return a
+        e_greedy = np.zeros(self.n_actions)
+        e_greedy.fill(self.epsilon / (self.n_actions - 1))
+        e_greedy[np.argmax(self.Q[state])] = 1 - self.epsilon
+        return np.random.choice(self.n_actions, p = e_greedy)
         
-    def update(self, state, action, reward):
-        # TO DO: Add own code
-        pass
+    def update(self, state, action, reward, stateprime, actionprime, alpha=0.1, gamma=1):
+        self.Q[state][action] = self.Q[state][action] + alpha*(reward + gamma*self.Q[stateprime][actionprime] - self.Q[state][action])
+
 
 class ExpectedSARSAAgent(object):
 
@@ -42,14 +42,16 @@ class ExpectedSARSAAgent(object):
         self.n_actions = n_actions
         self.n_states = n_states
         self.epsilon = epsilon
-        # TO DO: Add own code
-        pass
+        self.Q = np.zeros((n_states, n_actions))
         
     def select_action(self, state):
-        # TO DO: Add own code
-        a = random.choice(range(self.n_actions)) # Replace this with correct action selection
-        return a
+        e_greedy = np.zeros(self.n_actions)
+        e_greedy.fill(self.epsilon / (self.n_actions - 1))
+        e_greedy[np.argmax(self.Q[state])] = 1 - self.epsilon
+        return np.random.choice(self.n_actions, p = e_greedy)
         
-    def update(self, state, action, reward):
-        # TO DO: Add own code
-        pass
+    def update(self, state, action, reward, stateprime, alpha=0.1, gamma=1):
+        prob = np.zeros(self.n_actions)
+        prob.fill(self.epsilon / (self.n_actions - 1))
+        prob[np.argmax(self.Q[state])] = 1 - self.epsilon
+        self.Q[state][action] = self.Q[state][action] + alpha*(reward + sum([prob[a]*self.Q[stateprime][a] for a in range(self.n_actions)]) - self.Q[state][action])
--- ShortCutExperiment.py
+++ ShortCutExperiment.py
@@ -4,34 +4,128 @@ from ShortCutEnvironment import *
 import sys
 import matplotlib.pyplot as plt
 
-def run_repetitions():
-    n_episodes = 10000
-    n_rep = 100
-    res = np.empty((n_rep, n_episodes))
-    
-    for rep in range(n_rep):
-        env = ShortcutEnvironment()
-        agent = QLearningAgent(n_actions=env.action_size(), n_states=env.state_size(), epsilon=0.1)
-        
-        for episode in range(n_episodes):
-            sys.stdout.write("\r [" + "="*int(((episode+1+(rep*n_episodes))/(n_episodes*n_rep)) * 20) + "."*(20-(int(((episode+1+(rep*n_episodes))/(n_episodes*n_rep)) * 20))) +f"] {episode+1+(rep*n_episodes)}/{(n_episodes*n_rep)}")
-            sys.stdout.flush()
 
-            state = env.state()
-            while not env.done():
-                action = agent.select_action(state)
-                reward = env.step(action)
-                res[rep][episode] += reward
-                stateprime = env.state()
-                agent.update(state,action,reward,stateprime)
-                state = stateprime
-            env.reset()
-                
-    
+def render_policy(env, agent):
+    print()
+    for r in range(env.r):
+        for c in range(env.c):
+            if env.s[r][c] == "X":
+                a = np.argmax(agent.Q[r*env.c + c])
+                if a == 0:
+                    print("^",end="")
+                elif a==1:
+                    print("v",end="")
+                elif a==2:
+                    print("<",end="")
+                elif a==3:
+                    print(">",end="")
+            else:
+                print(env.s[r][c],end="")
+        print()
+    print()
+
+def plot(res):
     mean_res = res.mean(axis = 0)
     print(res,mean_res)
     plt.plot(mean_res)
     plt.show()
 
+def run_repetitions(
+        agent_type,
+        windy = False, 
+        n_episodes=10000,
+        n_rep = 100,
+        alpha = 0.1,
+        gamma = 1.0,
+        epsilon = 0.1):
+    
+    res = np.empty((n_rep, n_episodes))
+    env = WindyShortcutEnvironment() if windy else ShortcutEnvironment()
 
-run_repetitions()
+    if agent_type == "qlearning":
+
+        for rep in range(n_rep):
+            agent = QLearningAgent(n_actions=env.action_size(), n_states=env.state_size(), epsilon=epsilon)
+            for episode in range(n_episodes):
+                sys.stdout.write("\r [" + "="*int(((episode+1+(rep*n_episodes))/(n_episodes*n_rep)) * 20) + "."*(20-(int(((episode+1+(rep*n_episodes))/(n_episodes*n_rep)) * 20))) +f"] {episode+1+(rep*n_episodes)}/{(n_episodes*n_rep)}")
+                sys.stdout.flush()
+
+                # Initialize S
+                state = env.state()
+                # Loop for each step of episode, until S is terminal
+                while not env.done():
+                    # Choose A from S using egreedy derived from Q 
+                    action = agent.select_action(state)
+                    # Take action A, observe R, S'
+                    reward = env.step(action)
+                    res[rep][episode] += reward
+                    stateprime = env.state()
+                    # Q(S,A) = Q(S,A) + alpha(R + gamma*max_a(Q(S',A)) - Q(S,A))
+                    agent.update(state,action,reward,stateprime,alpha=alpha,gamma=gamma)
+                    # S = S'
+                    state = stateprime
+                # Reset environment
+                env.reset()  
+        
+
+    elif agent_type == "sarsa":
+        
+        for rep in range(n_rep):
+            agent = SARSAAgent(n_actions=env.action_size(), n_states=env.state_size(), epsilon=epsilon)
+            for episode in range(n_episodes):
+                sys.stdout.write("\r [" + "="*int(((episode+1+(rep*n_episodes))/(n_episodes*n_rep)) * 20) + "."*(20-(int(((episode+1+(rep*n_episodes))/(n_episodes*n_rep)) * 20))) +f"] {episode+1+(rep*n_episodes)}/{(n_episodes*n_rep)}")
+                sys.stdout.flush()
+
+                # Initialize S
+                state = env.state()
+                # Choose A from S using egreedy derived from Q 
+                action = agent.select_action(state)
+                # Loop for each step of episode, until S is terminal
+                while not env.done():
+                    # Take action A, observe R, S'
+                    reward = env.step(action)
+                    res[rep][episode] += reward
+                    stateprime = env.state()
+                    # Choose A' from S' using egreedy derived from Q
+                    actionprime = agent.select_action(stateprime)
+                    # Q(S,A) = Q(S,A) + alpha(R + gamma * Q(S',A') - Q(S,A))
+                    agent.update(state,action,reward,stateprime,actionprime,alpha=alpha,gamma=gamma)
+                    # S = S'
+                    state = stateprime
+                    # A = A'
+                    action = actionprime
+                # Reset environment
+                env.reset()      
+    
+    
+    elif agent_type == "expectedsarsa":
+
+        for rep in range(n_rep):
+            agent = ExpectedSARSAAgent(n_actions=env.action_size(), n_states=env.state_size(), epsilon=epsilon)
+            for episode in range(n_episodes):
+                sys.stdout.write("\r [" + "="*int(((episode+1+(rep*n_episodes))/(n_episodes*n_rep)) * 20) + "."*(20-(int(((episode+1+(rep*n_episodes))/(n_episodes*n_rep)) * 20))) +f"] {episode+1+(rep*n_episodes)}/{(n_episodes*n_rep)}")
+                sys.stdout.flush()
+
+                # Initialize S
+                state = env.state()
+                # Loop for each step of episode, until S is terminal
+                while not env.done():
+                    # Choose A from S using egreedy derived from Q 
+                    action = agent.select_action(state)
+                    # Take action A, observe R, S'
+                    reward = env.step(action)
+                    res[rep][episode] += reward
+                    stateprime = env.state()
+                    # Q(S,A) = Q(S,A) + alpha(R + gamma*sum_{A'}(p(A'|S')*Q(S',A')) - Q(S,A))
+                    agent.update(state,action,reward,stateprime,alpha=alpha,gamma=gamma)
+                    # S = S'
+                    state = stateprime
+                # Reset environment
+                env.reset()        
+           
+    return res
+
+
+def main():
+
+    pass
